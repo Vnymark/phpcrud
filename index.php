c00101e259085b066dd032d6011c5baf66f2747a
Viktor Nymark <vnymark@gmail.com>
2018-12-28 13:30:09 +0100

    Initial commit, started on the frontend.

---
+++ index.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.css"/>
+
+<div class="container">
+    <div class="content-header">
+        <h1>Profilinformation</h1>
+    </div>
+    <div class="inner-content">
+    </div>
+</div>
