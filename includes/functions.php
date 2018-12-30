bcc952584f76f4464572ec2fe403f4b2e0b2abcc
Viktor Nymark <vnymark@gmail.com>
2018-12-30 02:19:16 +0100

    Added better validation for creating a profile.

--- includes/functions.php
+++ includes/functions.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+ * Function for autoloading manually created classes.
+ * Will check if a file for the required class exists and require it.
+ */
 function classAutoload($class_name)
 {
     if (file_exists(  __DIR__ . '/includes/classes/' . $class_name . '.php')) {
@@ -7,8 +10,14 @@ function classAutoload($class_name)
     }
 }
 
+/*
+ * Make sure that the function for autoloading classes is included in the autoloader.
+ */
 spl_autoload_register('classAutoload');
 
+/*
+ * Validates an e-mail address according to the built in php filter.
+ */
 function isValidEmail($email){
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         return false;
@@ -16,21 +25,49 @@ function isValidEmail($email){
         return true;
     }
 }
-
+/*
+ * Validates name with letters between a to ö.
+ * Allows double names separated with hyphen.
+ */
 function isValidName($name){
-    if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+    if (!preg_match("/^[a-öA-Ö]*([-]?[a-öA-Ö])*$/", $name)) {
         return false;
     } else {
         return true;
     }
-
 }
 
+/*
+* Validates regular Swedish social security numbers to a certain level.
+* No special cases like temporary SSN's are allowed, or SSN's for businesses.
+* The regex will allow some faulty dates, but these should be handled by the checkdate().
+*/
 function isValidSSN($ssn){
-    if (!preg_match("/^[a-zA-Z ]*$/", $ssn)) {
+    if (!preg_match("/\b(((20)((0[0-9])|(1[0-8])))|(([1][^0-8])\d{2}))((0[1-9])|1[0-2])((0[1-9])|(1[0-9])|(2[0-9])|(3[0-1]))[-]?\d{4}?\b/", $ssn)) {
+        return false;
+    } else {
+        $year = substr($ssn, 0, 4);
+        $month = substr($ssn, 4, 2);
+        $date = substr($ssn, 6, 2);
+
+        if (!checkdate($month, $date, $year)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+/*
+ * Validates phone numbers according to Swedish phone numbers.
+ * A valid phone number should start with a 0( or +44 to +48).
+ * If the "second" digit is a 7, it will expect eight following digits. - For a total of 10 digits.
+ * If the "second" digit is anything else than a 7, it will allow six to eight following digits. - For a total of 8 to 10 digits.
+ */
+function isValidPhone($phone){
+    if (!preg_match("/^((0|([\+][4][4-8]))((7\d{8}?)|([^7]\d{6,8})))$/", $phone)) {
         return false;
     } else {
         return true;
     }
-
 }
--- index.php
+++ index.php
@@ -24,15 +24,23 @@
                     $errors[] = 'Du måste ange en korrekt e-postadress';
                 }
 
+                if (isset($_POST['ssn']) && !isValidSSN($_POST['ssn'])) {
+                    $errors[] = 'Du måste ange ett korrekt personnummer';
+                }
+
+                if (isset($_POST['phone']) && !isValidPhone($_POST['phone'])) {
+                    $errors[] = 'Du måste ange ett korrekt telefonnummer';
+                }
+
                 if (empty($errors)) {
                     $profile = new classes\Profile();
                     $profile->set('firstname',$_POST['firstname']);
                     $profile->set('lastname', $_POST['lastname']);
                     $profile->set('email', $_POST['email']);
-                    if (isset($_POST['ssn']) && $_POST['ssn'] !== null && isValidSSN($_POST['ssn'])) {
+                    if (isset($_POST['ssn'])) {
                         $profile->set('ssn', $_POST['ssn']);
                     }
-                    if (isset($_POST['phone']) && $_POST['phone'] !== null) {
+                    if (isset($_POST['phone'])) {
                         $profile->set('phone', $_POST['phone']);
                     }
 
@@ -45,12 +53,12 @@
             ?>
             <p>
                 <label for="firstname"><?php echo 'Förnamn'; ?></label><br/>
-                <input type="text" name="firstname" id="firstname" required="required"
+                <input type="text" name="firstname" id="firstname" minlength="2" required="required"
                        value="<?php echo isset($_POST['firstname']) ? htmlspecialchars($_POST['firstname']) : ''; ?>"/>
             </p>
             <p>
                 <label for="lastname"><?php echo 'Efternamn'; ?></label><br/>
-                <input type="text" name="lastname" id="lastname" required="required"
+                <input type="text" name="lastname" id="lastname" minlength="2" required="required"
                        value="<?php echo isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : ''; ?>"/>
             </p>
             <p>
@@ -59,15 +67,14 @@
                        value="<?php echo isset($_POST['email']) ? htmlspecialchars($_POST['email']) : ''; ?>"/>
             </p>
             <p>
-                <label for="ssn"><?php echo 'Personnummer (exempel: ÅÅMMDDXXXX):'; ?></label><br/>
-                <input type="text" id="ssn" name="ssn" maxlength="10"
+                <label for="ssn"><?php echo 'Personnummer (exempel: ÅÅÅÅMMDDXXXX)'; ?></label><br/>
+                <input type="text" name="ssn" id="ssn" minlength="12" maxlength="12"
                        value="<?php echo isset($_POST['ssn']) ? htmlspecialchars($_POST['ssn']) : ''; ?>"/>
             </p>
             <p>
                 <label for="phone"><?php echo 'Telefonnummer'; ?></label><br/>
-                <input type="tel" id="phone"
-                       value="<?php echo isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : ''; ?>"
-                       name="phone"/>
+                <input type="tel" name="phone" id="phone" maxlength="10"
+                       value="<?php echo isset($_POST['phone']) ? htmlspecialchars($_POST['phone']) : ''; ?>"/>
             </p>
             <button type="submit" name="cancel" value="1" class="btn btn-danger">&nbsp;
                 Avbryt
